03f0ee4bb573b17e3d3bbc91bc75c48321d3989d
Laurentiu Dancu <laue.dancu@gmail.com>
2025-05-31 17:03:17 +0300

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   site: 'https://ghilda.ro',
   integrations: [mdx(), tailwind(), react()],
   output: 'server',
+  adapter: netlify(),
 });
